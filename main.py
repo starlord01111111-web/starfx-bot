84ccd7d810c5592c3146bd3d077f12ed18750f5d
starlord01111111-web <starlord01111111@gmail.com>
2026-09-11 07:59:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,9 @@ class HealthCheckHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b"OK")
-
+def do_HEAD(self):
+        self.send_response(200)
+        self.end_headers()
 def start_dummy_server():
     port = int(os.environ.get("PORT", 10000))
     server = HTTPServer(("0.0.0.0", port), HealthCheckHandler)
