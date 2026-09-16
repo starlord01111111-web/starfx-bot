1aa75009f52b85409640c910a5cd48398da2518d
starlord01111111-web <starlord01111111@gmail.com>
2026-09-16 09:53:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
 # ============================== CONFIG ==============================
-TELEGRAM_TOKEN   = "8656945768:AAGWePpyEbPmn8CYySpJedPOHc6LaNWHpkg"
+TELEGRAM_TOKEN   = "8656945768:AAHGUJbf-ad1Pe57jKqvqp-CjOYPXQfG1iA"
 TELEGRAM_CHAT_ID = "-1004365660319"
 DERIV_APP_ID     = os.environ.get("DERIV_APP_ID", "1089")
 
